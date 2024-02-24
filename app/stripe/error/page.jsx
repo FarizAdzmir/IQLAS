4301ed840a086fdbbb3d160345f5e6476c1f29d8
Fariz <farizadzmir@thecrate.in>
2024-02-25 03:07:19 +0800

    Beta Release V.0.5

--- app/stripe/error/page.jsx
+++ app/stripe/error/page.jsx
@@ -1,9 +1,9 @@
 import Link from 'next/link';
 const ErrorPage = () => {
   return (
-    <section className='py-72'>
+    <section className='py-72 bg-primary'>
       <div className='container mx-auto'>
-        <h3 className='text-center mb-4'>Something went wrong!</h3>
+        <h3 className='text-center mb-4 text-accent'>Something went wrong!</h3>
         <Link href='/'>
           <button className='btn btn-accent mx-auto'>
             Back to the homepage
--- app/stripe/success/page.jsx
+++ app/stripe/success/page.jsx
@@ -2,13 +2,13 @@ import Link from 'next/link';
 
 const SuccessPage = () => {
   return (
-    <section className='py-72'>
+    <section className='py-72 bg-primary'>
       <div className='container mx-auto'>
-        <h3 className='text-center mb-4'>
+        <h3 className='text-center mb-4 text-accent'>
           Your payment was successful! Thank you!
         </h3>
         <Link href='/'>
-          <button className='btn btn-primary mx-auto'>
+          <button className='btn btn-accent mx-auto'>
             Back to the homepage
           </button>
         </Link>

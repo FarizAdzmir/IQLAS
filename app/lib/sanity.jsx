c352e51ddbdbb07db8141aca41e91fa9434dac83
Fariz <farizadzmir@gmail.com>
2025-01-31 00:41:32 +0800

    Release V.0.7

--- app/lib/sanity.jsx
+++ app/lib/sanity.jsx
@@ -2,9 +2,9 @@ import { createClient } from 'next-sanity';
 import ImageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-  projectId: '0glasetj',
+  projectId: '8csus0c5',
   dataset: 'iqlas-store',
-  apiVersion: '2024-02-25',
+  apiVersion: '2024-12-01',
   useCdn: true,
 });
 
